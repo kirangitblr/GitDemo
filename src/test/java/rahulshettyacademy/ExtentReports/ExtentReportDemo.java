8dd510757250da2404375a59b23a7468ac6f5084
Kiran J <kiranblrj@gmail.com>
2024-07-14 06:53:37 +0530

    first commit

---
+++ src/test/java/rahulshettyacademy/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,41 @@
+package rahulshettyacademy.ExtentReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReportDemo {
+	ExtentReports extent;
+
+	@BeforeTest
+	public void config() {
+		// ExtentReports, ExtentSparkReport
+		String path = System.getProperty("user.dir") + "\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web automation results");
+		reporter.config().setDocumentTitle("Test Results");
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Kiran J");
+	}
+
+	@Test
+	public void initialDemo() {
+		ExtentTest test = extent.createTest("intialDemo");
+		WebDriverManager.chromedriver();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("Result do not Match");
+		extent.flush();
+
+	}
+}
